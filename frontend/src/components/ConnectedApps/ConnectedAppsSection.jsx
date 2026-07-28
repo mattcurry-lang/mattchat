f7d5b0df5e2e15e6f6953d664c723d81ab593bc0
Mathew mutai <mathewmutai99@gmail.com>
2026-07-28 22:36:25 +0300

    Remove unused AnimatePresence import

--- frontend/src/components/ConnectedApps/ConnectedAppsSection.jsx
+++ frontend/src/components/ConnectedApps/ConnectedAppsSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import ConnectedAppCard from './ConnectedAppCard'
 import InstagramView from './InstagramView'
 import { useInstagramConnection } from '../../hooks/useInstagramConnection'
