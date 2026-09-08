4754b0c4c148bca26b3dbabbe79099be0e385f1b
Mathew mutai <mathewmutai99@gmail.com>
2026-09-08 19:47:48 +0300

    Fix import statement and export default in MusicSearch

--- frontend/src/components/Pulse/Music/MusicSearch.jsx
+++ frontend/src/components/Pulse/Music/MusicSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-import MusicService from '../../../lib/music/MusicService'
+import { MusicService } from '../../../lib/music/MusicService'
 import { useMusicPlayer } from '../../context/MusicPlayerContext'
 import { IconSearch, IconPlay, IconPause, IconMusic } from '../../Icons'
 
@@ -173,4 +173,3 @@ export default function MusicSearch({ autoFocus = false }) {
     </div>
   )
 }
-export default MusicService;
