41c77d7dc06983721118d76155ed86e44849602e
Mathew mutai <mathewmutai99@gmail.com>
2026-09-08 19:25:31 +0300

    Update import path for MusicPlayerContext

--- frontend/src/components/Pulse/Music/MiniPlayer.jsx
+++ frontend/src/components/Pulse/Music/MiniPlayer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useMusicPlayer } from '../../../context/MusicPlayerContext'
+import { useMusicPlayer } from '../../context/MusicPlayerContext'
 import { IconPlay, IconPause, IconMusic, IconX, IconChevronUp } from '../../Icons'
 
 /**
