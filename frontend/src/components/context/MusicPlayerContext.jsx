4acedda28f02eab9b7d00be6fb45d7dbd4715b28
Mathew mutai <mathewmutai99@gmail.com>
2026-09-12 09:50:20 +0300

    Update import paths in MusicPlayerContext.jsx

--- frontend/src/components/context/MusicPlayerContext.jsx
+++ frontend/src/components/context/MusicPlayerContext.jsx
@@ -7,18 +7,18 @@ import BecomeArtistModal from './BecomeArtistModal'
 import ArtistDashboard from './ArtistDashboard'
 import LibraryPanel from './LibraryPanel'
 import { useMusicPlayer } from '../../context/MusicPlayerContext'
-import { MusicService } from '../../../lib/music/MusicService'
-import { YouTubeMusicProvider } from '../../../lib/music/providers/YouTubeMusicProvider'
-import { ArtistService } from '../../../lib/music/ArtistService'
-import { PlaylistService } from '../../../lib/music/PlaylistService'
-import { useDominantColor, rgba } from '../../../lib/music/extractColor'
-import { useMusicColors } from '../../../hooks/useMusicColors'
+import { MusicService } from '../../lib/music/MusicService'
+import { YouTubeMusicProvider } from '../../lib/music/providers/YouTubeMusicProvider'
+import { ArtistService } from '../../lib/music/ArtistService'
+import { PlaylistService } from '../../lib/music/PlaylistService'
+import { useDominantColor, rgba } from '../../lib/music/extractColor'
+import { useMusicColors } from '../../hooks/useMusicColors'
 import {
   IconMusic, IconX, IconPlay, IconPause, IconMic, IconSearch, IconListMusic,
   IconSkipBack, IconSkipForward, IconShuffle, IconRepeat, IconHeart, IconVolume2, IconPlus,
   IconQueueList, IconLyrics, IconShare2, IconLoader2,
 } from '../../Icons'
-import { useIsMobile } from '../../../hooks/useIsMobile'
+import { useIsMobile } from '../../hooks/useIsMobile'
 
 function greeting() {
   const h = new Date().getHours()
